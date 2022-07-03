4e11b0b92995242d9e4ae2056195c624651b37dd
unknown <mdismailhossain215@gmail.com>
2022-07-03 07:31:52 +0600

    Student and Dog Class

---
+++ src/OOPProject001/DogOOP.java
@@ -0,0 +1,80 @@
+package OOPProject001;
+class Dog{
+	  
+	  int legs;
+	  String color;
+	  float weight;
+	  String food;
+	  boolean isPet;
+	  
+	  void eat(){
+	    //This is a funciton also known as method
+	  }
+	  
+	  void sleep() {
+
+	    //This is a funciton also known as method
+	    
+	  } 
+
+	  
+	  public Dog(){
+	    //default constructor
+	    System.out.println("The Dog()constructor is called");
+	    legs = 4;
+	    color = "yellow";
+	    weight = 20;
+	    food = "rice";
+	    isPet = true;
+	  }
+	//Method overloading
+	  
+	  public Dog(int l, String col, float weight){
+	    //declaring parameter
+	    
+	    legs = l;
+	    color = col;
+	    this.weight = weight;//if parameter is in same name
+	    
+	    //System.out.println("\n Dog has "+ l + " legs, with " + l + " color and weight is "+ weight +"\n");
+	        
+	  }  
+	  @Override
+	  public String toString(){
+
+	    //toString method must declare default method
+	    
+	    return "legs = " + legs + ", color " + color + " and weight is "+ weight ;
+	  }
+	 
+	}
+
+
+	
+
+public class DogOOP {
+
+	public static void main(String[] args) {
+//			int s2;
+//		    s2 = 20;
+//		    int s1 = 10;
+		    
+		    Dog d4; //creating object
+		    d4 = new Dog(); //declaring object
+		    
+		    Dog d3 = new Dog(4, "White", 15);//entering the value of parameter
+		    //d4.color = "black";
+		    
+		    d3.isPet = true;
+		    System.out.println(d3);
+		    
+		    System.out.println("The dog's color is " + d4.color);
+		    System.out.println("The dog has " + d4.legs + " legs");
+		    System.out.println("The dog eat " + d4.food);
+		    System.out.println("The dog is a " + d4.isPet );
+
+		    //d3.Dog(4, "White", 15);
+
+	}
+
+}
---
+++ src/OOPProject001/StudentTeacherOOP.java
@@ -0,0 +1,77 @@
+package OOPProject001;
+
+class NStudent{
+	  String name;
+	  int idNumber;
+	  boolean isRegular;
+	  double cgpa;
+	  
+	  void study(){
+	    
+	  }	
+	  
+	  void research(){
+	    
+	  }
+	  
+	  public NStudent() {
+
+	    //default constructor
+	    
+	  }
+	  
+	  public NStudent(String N, int ID, double CGPA){
+	    //overloading constructor
+	    this.name = N;
+	    this.idNumber = ID;
+	    this.cgpa = CGPA;    
+	  }
+	  
+	  @Override
+	  public String toString(){
+	    return "Student's Name is " + name + ", ID is " + idNumber + ", CGPA is " + cgpa;
+	  }	  
+	}
+
+	class Teacher {
+		String designation;
+		int regNo;
+		int classNumber;
+		
+		void teach() {
+			
+		}
+	
+	public Teacher() {
+	//default constructor	
+	}
+	
+	public Teacher (String D, int R, int C) {
+		//overloading constructor
+		this.designation = D;
+		this.regNo = R;
+		this.classNumber = C;
+		System.out.println("Designation is " + designation + ", Registration Number is " + regNo + " and classNumber is " + classNumber);
+	}
+	}
+	
+public class StudentTeacherOOP {
+	
+	
+	public static void main(String[] args) {
+		
+		NStudent s1 = new NStudent("Ismail", 20123811, 3.81);
+		System.out.println(s1);
+		
+		NStudent s2;
+		s2 = new NStudent();
+		s2.idNumber = 20123811;
+		System.out.println("ID number of the s2 is " + s2);
+		
+		NStudent s3 = new NStudent("Shihab", 20123920, 3.69);
+		System.out.println(s3);
+		
+		Teacher t1 = new Teacher("Lecturer", 2001, 5);
+		System.out.println(t1);
+	}
+}
