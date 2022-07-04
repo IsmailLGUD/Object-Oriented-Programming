1f78da1b989779f33b3d3e6a7e68cfbc95e1a015
unknown <mdismailhossain215@gmail.com>
2022-07-04 06:47:40 +0600

    Encapsulation Testing

---
+++ src/Encapsulation/EncapTest.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class EncapTest {
+
+	public static void main(String[] args) {
+		
+		Person p1 = new Person();
+		p1.setName("Ismail");
+		p1.setAge(22);
+		System.out.println("Name: "+ p1.getName() + " and Age is " + p1.getAge());
+		
+	}
+
+}
---
+++ src/Encapsulation/Person.java
@@ -0,0 +1,25 @@
+package Encapsulation;
+
+public class Person {
+	
+	private String name;
+	private int age;
+	
+	void setName(String N) {
+		name = N;
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	void setAge(int A) {
+		age = A;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+}
